167786010d537134155a455b27bb0a4688c826e2
rhernandez1302 <104451977+rhernandez1302@users.noreply.github.com>
2023-02-24 15:05:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+"""Functions subtracts water from resources"""
 def water(choice):
     if choice == 'espresso':
         return total_water - 50
@@ -35,7 +36,7 @@ def water(choice):
         return total_water - 200
     elif choice == 'cappuccino':
         return total_water - 250
-
+"""Functions subtracts milk from resources"""
 def milk(choice):
     if choice == 'espresso':
         return total_milk
@@ -43,7 +44,7 @@ def milk(choice):
         return total_milk - 150
     elif choice == 'cappuccino':
         return total_milk - 100
-
+"""Functions subtracts coffee from resources"""
 def coffee(choice):
     if choice == 'espresso':
         return total_coffee - 18
@@ -51,20 +52,20 @@ def coffee(choice):
         return total_coffee - 24
     elif choice == 'cappuccino':
         return total_coffee - 24
-
+"""Functions calculates total amount payed """
 def payment(quarters, dimes, nickels, pennies):
     total_quarters = quarters * 0.25
     total_dimes = dimes * 0.10
     total_nickels = nickels * 0.05
     total_pennies = pennies * 0.01
     return total_quarters + total_pennies + total_nickels + total_dimes
-
+"""Functions returns the total cost of choice"""
 def cost(choice):
     return MENU[choice]['cost']
-
+"""Functions returns the current report on resources"""
 def report(total_water,total_milk,total_coffee):
     print(f"Water: {total_water}\nMilk: {total_milk}\nCoffee: {total_coffee}")
-
+"""Functions returns user what resources is not enough to make selected choice"""
 def enough_resources(total_water, total_milk, total_coffee):
     if total_water <= 0:
         print("Sorry not enough water")
@@ -76,7 +77,7 @@ def enough_resources(total_water, total_milk, total_coffee):
 total_water = resources['water']
 total_milk = resources['milk']
 total_coffee = resources['coffee']
-
+"""Added this while loop to keep running program"""
 machine_works = True
 while machine_works:
     choice = input("What would you like to order(espresso/latte/cappuccino) ")
@@ -86,6 +87,7 @@ while machine_works:
         total_water = water(choice)
         total_milk = milk(choice)
         total_coffee = coffee(choice)
+        """I did < 0 because if it equaled to 0, it means there was just enough resources to make the selected choice"""
         if total_water < 0 or total_milk < 0 or total_coffee < 0:
             enough_resources(total_water, total_milk, total_coffee)
         else:
