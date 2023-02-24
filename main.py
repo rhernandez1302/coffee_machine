4d5b04af895a4f0b7cb17cbbc900463736ac9cb7
rhernandez1302 <104451977+rhernandez1302@users.noreply.github.com>
2023-02-24 14:37:04 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,103 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+def water(choice):
+    if choice == 'espresso':
+        return total_water - 50
+    elif choice == 'latte':
+        return total_water - 200
+    elif choice == 'cappuccino':
+        return total_water - 250
+
+def milk(choice):
+    if choice == 'espresso':
+        return total_milk
+    elif choice == 'latte':
+        return total_milk - 150
+    elif choice == 'cappuccino':
+        return total_milk - 100
+
+def coffee(choice):
+    if choice == 'espresso':
+        return total_coffee - 18
+    elif choice == 'latte':
+        return total_coffee - 24
+    elif choice == 'cappuccino':
+        return total_coffee - 24
+
+def payment(quarters, dimes, nickels, pennies):
+    total_quarters = quarters * 0.25
+    total_dimes = dimes * 0.10
+    total_nickels = nickels * 0.05
+    total_pennies = pennies * 0.01
+    return total_quarters + total_pennies + total_nickels + total_dimes
+
+def cost(choice):
+    return MENU[choice]['cost']
+
+def report(total_water,total_milk,total_coffee):
+    print(f"Water: {total_water}\nMilk: {total_milk}\nCoffee: {total_coffee}")
+
+def enough_resources(total_water, total_milk, total_coffee):
+    if total_water <= 0:
+        print("Sorry not enough water")
+    elif total_coffee <= 0:
+        print("NSorry not coffee")
+    elif total_milk <= 0:
+        print("Sorry not milk")
+
+total_water = resources['water']
+total_milk = resources['milk']
+total_coffee = resources['coffee']
+
+machine_works = True
+while machine_works:
+    choice = input("What would you like to order(espresso/latte/cappuccino) ")
+    if choice == 'report':
+        report(total_water, total_milk, total_coffee)
+    else:
+        total_water = water(choice)
+        total_milk = milk(choice)
+        total_coffee = coffee(choice)
+        if total_water < 0 or total_milk < 0 or total_coffee < 0:
+            enough_resources(total_water, total_milk, total_coffee)
+        else:
+            owe = cost(choice)
+            print(f"Your {choice} will be ${owe}")
+            quarters = int(input("How many quarters? "))
+            dimes = int(input("How many dimes? "))
+            nickels = int(input("How many nickels? "))
+            pennies = int(input("How many pennies? "))
+            payed = payment(quarters, dimes, nickels, pennies)
+            if payed < owe:
+                print(f"Sorry that is not enough to pay for your {choice}")
+            else:
+                change = round(payed - owe, 2)
+                print(f"You payed ${payed}, your change is ${change}. Enjoy!")
